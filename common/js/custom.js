9b76d88076549ef8b720a8d2d39907c14c101994
Igor C. A. de Lima <igorcadelima@gmail.com>
2015-10-09 13:09:34 -0300

    Adiciona Google Analytics code à todas as aulas

--- common/js/custom.js
+++ common/js/custom.js
@@ -7,3 +7,32 @@ $(function() {
   });
 
 });
+
+function loadScript( url, callback ) {
+  var script = document.createElement( "script" )
+  script.type = "text/javascript";
+  if(script.readyState) {  //IE
+    script.onreadystatechange = function() {
+      if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  //Others
+    script.onload = function() {
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.body.appendChild( script );
+}
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+var analyticsUrl = gaJsHost + "google-analytics.com/ga.js";
+loadScript(pathtoscript, function() {
+	try {
+		var pageTracker = _gat._getTracker("UA-66547978-1");
+		pageTracker._trackPageview();
+	} catch (err) {}
+});
